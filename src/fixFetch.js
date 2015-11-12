6be0287deb56acce06534b3ec5bf8ba8a51c208c
Denis Nedelyaev <denvned@gmail.com>
2015-11-12 08:43:54 +0300

    Replace node-fetch with isomorphic-fetch, make it a dependency

--- src/fixFetch.js
+++ src/fixFetch.js
@@ -1,10 +1,12 @@
+import fetch from 'isomorphic-fetch';
+
 if (typeof self == 'undefined') {
     const {prototype} = module.constructor;
     const {require} = prototype;
 
     prototype.require = function(path) {
         if (path == 'fbjs/lib/fetch' || path == 'fbjs/lib/fetchWithRetries') {
-            path = 'node-fetch';
+            return fetch;
         }
         return require.call(this, path);
     };
