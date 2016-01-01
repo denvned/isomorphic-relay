f3face069136d7e29807b82eeeda9256a92b6ccd
Denis Nedelyaev <denvned@gmail.com>
2016-01-01 14:25:44 +0300

    Upgrade to babel v6

--- examples/star-wars/webpack.config.js
+++ examples/star-wars/webpack.config.js
@@ -1,10 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: [
-      'babel-core/polyfill',
-      path.resolve(__dirname, 'lib', 'client.js'),
-    ],
+    entry: path.resolve(__dirname, 'lib', 'client.js'),
     output: {
         filename: 'app.js',
         path: path.resolve(__dirname, 'lib'),
